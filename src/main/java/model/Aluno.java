7f24f1d2129c5fd623d8ea94959da50322165c63
guilhermemhz <gmhoeller1@gmail.com>
2025-11-12 11:20:09 -0300

    Add Javadoc documentation to Aluno class

--- src/main/java/model/Aluno.java
+++ src/main/java/model/Aluno.java
@@ -1,47 +1,98 @@
 package model;
 
+/**
+ * Classe que representa um aluno do sistema.
+ * 
+ * <p>Herda os atributos básicos de {@link Pessoa}, como ID, nome, idade e CPF,
+ * e adiciona informações específicas de alunos, como curso e fase.</p>
+ * 
+ * @author Guilhermemhz
+ */
 
 public class Aluno extends Pessoa {
 
-    // Atributos
+    /** Nome do curso em que o aluno está matriculado. */
     private String curso;
+    
+    /** Fase (ou semestre) atual do aluno. */
     private int fase;
 
-    // Método Construtor de Objeto Vazio
+    /**
+     * Construtor padrão sem parâmetros.
+     * Cria um objeto {@code Aluno} vazio.
+     */
     public Aluno() {
     }
 
-    // Método Construtor de Objeto, inserindo dados
+    /**
+     * Construtor que inicializa apenas os atributos específicos de {@code Aluno}.
+     *
+     * @param curso nome do curso em que o aluno está matriculado
+     * @param fase fase (ou semestre) atual do aluno
+     */
     public Aluno(String curso, int fase) {
         this.curso = curso;
         this.fase = fase;
     }
 
-    // Método Construtor usando tamb�m o construtor da SUPERCLASSE
+    /**
+     * Construtor que inicializa todos os atributos, incluindo os herdados de {@link Pessoa}.
+     *
+     * @param curso nome do curso em que o aluno está matriculado
+     * @param fase fase (ou semestre) atual do aluno
+     * @param id identificador único do aluno
+     * @param nome nome completo do aluno
+     * @param idade idade em anos
+     * @param CPF CPF do aluno
+     */
     public Aluno(String curso, int fase, int id, String nome, int idade, String CPF) {
         super(id, nome, idade, CPF);
         this.curso = curso;
         this.fase = fase;
     }
 
-    // Métodos GET e SET
+    /**
+     * Retorna o nome do curso em que o aluno está matriculado.
+     * 
+     * @return o nome do curso
+     */
     public String getCurso() {
         return curso;
     }
 
+    /**
+     * Define o nome do curso em que o aluno está matriculado.
+     * 
+     * @param curso novo nome do curso
+     */
     public void setCurso(String curso) {
         this.curso = curso;
     }
-
+    
+    /**
+     * Retorna a fase (ou semestre) atual do aluno.
+     * 
+     * @return a fase atual do aluno
+     */
     public int getFase() {
         return fase;
     }
 
+    /**
+     * Define a fase (ou semestre) atual do aluno.
+     * 
+     * @param fase nova fase do aluno
+     */
     public void setFase(int fase) {
         this.fase = fase;
     }
 
-    // Override necessário para poder retornar os dados de Pessoa no toString para aluno.
+    /**
+     * Retorna uma representação textual do aluno,
+     * incluindo informações herdadas da classe {@link Pessoa}.
+     * 
+     * @return uma string contendo os dados do aluno
+     */
     @Override
     public String toString() {
         return "\n ID: " + this.getId()
@@ -51,6 +102,5 @@ public class Aluno extends Pessoa {
                 + "\n Fase:" + this.getFase()
                 + "\n -----------";
     }
-
-   
+  
 }
