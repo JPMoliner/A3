82d8d6a7a91ff20163c884f416bdb10b4e4f260f
João Pedro Moliner <129806580+JPMoliner@users.noreply.github.com>
2025-11-16 19:41:29 -0300

    Add JavaDoc comments to Mensagens view

--- src/main/java/view/Mensagens.java
+++ src/main/java/view/Mensagens.java
@@ -1,7 +1,15 @@
 package view;
 
+/**
+ * Exceção específica para encapsular mensagens exibidas na camada de visão.
+ */
 public class Mensagens extends Exception {
 
+     /**
+     * Cria uma nova instância da exceção contendo a mensagem informada.
+     *
+     * @param msg texto descritivo que será apresentado ao usuário
+     */
     Mensagens(String msg) {
         super(msg);
     }
